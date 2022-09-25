0db5ef47891e68e69870801106da136f31b105bb
Akshat Mani Tripathi <akshatmani1010@gmail.com>
2022-09-25 12:58:00 +0530

    final commit

---
+++ lottery-react/src/App.js
@@ -0,0 +1,103 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
+
+class App extends React.Component {
+  state = {
+    manager: '',
+    players:[],
+    balance:'',
+    value:'',
+    message:''
+  };
+
+  async componentDidMount(){
+      const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
+      this.setState({manager,players,balance});
+  }
+
+
+
+  onSubmit = async(event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({message: 'waiting in transaction success....'})
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value , 'ether')
+    })
+
+    this.setState({message: 'you have been entered!'})
+  }
+
+  onClick = async() => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({message: 'waiting on transaction success....'})
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    })
+
+    this.setState({message: 'A winner has been picked!'});
+  }
+
+
+  render() {
+
+    const myLotteryStyle = {
+      color: "white",
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      fontFamily: "Arial"
+    }
+    const infoStyle = {
+      padding: "40px 25px",
+      fontFamily: "Arial",
+      margin: "40px",
+    }
+
+    const h4Style = {
+      padding: "10px",
+      fontFamily: "Arial"
+    }
+
+    
+
+    return (
+      <div>
+        <center><h2>Lottery Contest</h2></center>
+         <p style={infoStyle}>this Contract is managed by {this.state.manager}
+         there are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance , 'ether')} ether!
+         </p>
+         <hr />
+         <form onSubmit={this.onSubmit}>
+          <h4 style={h4Style}>
+            Want to try your luck?
+          </h4>
+          <div>
+            <label style={h4Style}>Enter Amount of ether to enter</label><br/>
+            <br/>
+            <input style={h4Style}
+            value={this.state.value}
+            onChange={event => this.setState({ value: event.target.value})}
+            />
+          </div>
+          <button>Enter</button>
+         </form>
+         <hr />
+          <h4>Ready to pick winner?</h4>
+          <button onClick={this.onClick}>Pick a Winner!</button>
+         <hr />
+         <h1>{this.state.message}</h1>
+         <p>Note : You need metamask installed to enter the contest</p>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ lottery/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'panther prosper hockey fun dial practice hard fortune grab soldier organ panda',
+  // remember to change this to your own phrase!
+  'https://goerli.infura.io/v3/60e634ec236e4b0593b57621876178f2'
+  // remember to change this to your own endpoint!
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
